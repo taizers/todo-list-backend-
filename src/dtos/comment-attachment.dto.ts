bf17a75c7bf6be247861dbde7c493291e1ea6070
taizers <sasha.sachok@gmail.com>
2022-12-04 23:12:46 +0300

    change date format for attachments

--- src/dtos/comment-attachment.dto.ts
+++ src/dtos/comment-attachment.dto.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import moment from 'moment';
+
 import { commentAttachmentsPath } from '../constants';
 import { CommentAttachmentType } from '../types/entities/global.entities.type';
 
@@ -20,6 +22,6 @@ export default class CommentAttachmentDto {
       `${process.env.BACKEND_URL}${commentAttachmentsPath}/${model.name}` : '';
     this.name = model.name;
     this.type = model.type;
-    this.created_at = model.created_at;
+    this.created_at = moment(model.created_at).format('YYYY-MM-DD[T]HH:mm:ss.SSS');
   }
 }
--- src/dtos/task-attachment.dto.ts
+++ src/dtos/task-attachment.dto.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import moment from 'moment';
+
 import { taskAttachmentsPath } from '../constants';
 import { TaskAttachmentType } from '../types/entities/global.entities.type';
 
@@ -20,6 +22,6 @@ export default class TaskAttachmentDto {
       `${process.env.BACKEND_URL}${taskAttachmentsPath}/${model.name}` : '';
     this.name = model.name;
     this.type = model.type;
-    this.created_at = model.created_at.toString().slice(0, -1);
+    this.created_at = moment(model.created_at).format('YYYY-MM-DD[T]HH:mm:ss.SSS');
   }
 }
